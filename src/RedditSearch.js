d515e3b0ec1d664c07ecc3e71f2d984ae9deabbb
Mito6211 <willmoore674@gmail.com>
2021-03-02 23:08:32 -0500

    Vastly improved infinite scroll, much more consistent and stable. Refactor.

--- src/RedditSearch.js
+++ src/RedditSearch.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import RedditCard from "./RedditCard";
 import { v4 as uuidv4 } from "uuid";
 import debounce from "lodash.debounce";
@@ -12,45 +12,48 @@ export default function RedditSearch() {
   );
   const [postList, setPostList] = useState([]);
 
-  const fetchPosts = async (params = "", prevPosts = []) => {
-    const newPosts = [...prevPosts];
-    try {
-      const res = await fetch(
-        `https://www.reddit.com/r/${query}.json${params}`
-      );
-      const {
-        data: { after, children },
-      } = await res.json();
-
-      setAfterToken(after);
-
-      children.forEach((child) => {
+  const fetchPosts = useCallback(
+    async (params = "", prevPosts = []) => {
+      const newPosts = [...prevPosts];
+      try {
+        const res = await fetch(
+          `https://www.reddit.com/r/${query}.json${params}`
+        );
         const {
-          title,
-          subreddit_name_prefixed,
-          score,
-          permalink,
-          url,
-          selftext,
-        } = child.data;
+          data: { after, children },
+        } = await res.json();
+        console.log(after);
+        setAfterToken(after);
 
-        if (selftext.length === 0) {
-          newPosts.push({
-            id: uuidv4(),
+        children.forEach((child) => {
+          const {
             title,
             subreddit_name_prefixed,
             score,
             permalink,
             url,
-          });
-        }
-      });
-    } catch (err) {
-      console.error(err);
-    }
+            selftext,
+          } = child.data;
 
-    return newPosts;
-  };
+          if (selftext.length === 0) {
+            newPosts.push({
+              id: uuidv4(),
+              title,
+              subreddit_name_prefixed,
+              score,
+              permalink,
+              url,
+            });
+          }
+        });
+      } catch (err) {
+        console.error(err);
+      }
+
+      return newPosts;
+    },
+    [query]
+  );
 
   const search = async (e) => {
     e.preventDefault();
@@ -62,24 +65,40 @@ export default function RedditSearch() {
     setPostList(posts);
   };
 
-  const getMorePosts = async () => {
-    const afterParam = afterToken.length > 0 ? `?after=${afterToken}` : "";
+  const getMorePosts = useCallback(async () => {
+    try {
+      const afterParam = afterToken.length > 0 ? `?after=${afterToken}` : "";
 
-    setIsLoadingAdditionalPosts(true);
-    const posts = await fetchPosts(afterParam, postList);
-    setIsLoadingAdditionalPosts(false);
-    setPostList(posts);
-  };
-
-  window.onscroll = debounce(async (e) => {
-    const subredditExists = query.length > 0;
-    const isScrolledToBottomOfPage =
-      window.innerHeight + window.pageYOffset >= document.body.offsetHeight;
-
-    if (subredditExists && isScrolledToBottomOfPage) {
-      getMorePosts();
+      setIsLoadingAdditionalPosts(true);
+      const posts = await fetchPosts(afterParam, postList);
+      setIsLoadingAdditionalPosts(false);
+      setPostList(posts);
+    } catch {
+      console.error("Failed to get more posts");
     }
-  }, 1000);
+  }, [afterToken, fetchPosts, postList]);
+
+  useEffect(() => {
+    const refreshDebounce = debounce(async (e) => {
+      const subredditExists = query.length > 0;
+
+      // innerHeight is how tall the window is (constant value if window size doesn't change).
+      // pageYOffset is how far down the user has scrolled (starts at 0, ends at pageYOffset - innerHeight).
+      // offsetHeight is the total height of the page, not just the window height.
+      const isScrolledToBottomOfPage =
+        window.innerHeight + window.pageYOffset >= document.body.offsetHeight;
+
+      if (subredditExists && isScrolledToBottomOfPage) {
+        getMorePosts();
+      }
+    }, 1000);
+
+    document.addEventListener("scroll", refreshDebounce);
+
+    return () => {
+      document.removeEventListener("scroll", refreshDebounce);
+    };
+  }, [getMorePosts, query]);
 
   return (
     <div className="container">
@@ -104,7 +123,6 @@ export default function RedditSearch() {
         {isLoadingInitialPosts
           ? "Loading..."
           : postList.map((post) => <RedditCard key={post.id} data={post} />)}
-        <button onClick={getMorePosts}>Refresh</button>
         {isLoadingAdditionalPosts && "Loading More Posts..."}
       </div>
     </div>
